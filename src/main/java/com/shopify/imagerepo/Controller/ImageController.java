ebebb7f14e49542d27427732dccf70b56b6f2280
zhe <zhe.ji@carleton.ca>
2021-04-28 17:15:15 -0400

    Fix the build

--- src/main/java/com/shopify/imagerepo/Controller/ImageController.java
+++ src/main/java/com/shopify/imagerepo/Controller/ImageController.java
@@ -1,15 +1,11 @@
 package com.shopify.imagerepo.Controller;
 
-import com.shopify.imagerepo.Repository.ImageRepository;
-import com.shopify.imagerepo.Service.MapValidationService;
 import com.shopify.imagerepo.Service.ParseFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
